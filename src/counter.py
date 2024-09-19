489a5a7093fddb5b8054336d97880c7269afa478
root <root@NODE>
2024-09-19 03:33:25 -0700

    Adding tdd-main and workflow file into the repository.

---
+++ src/counter.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from . import status
+
+app = Flask(__name__)
+
+
+
+COUNTERS = {}
+
+# We will use the app decorator and create a route called slash counters.
+# specify the variable in route <name>
+# let Flask know that the only methods that is allowed to called
+# on this function is "POST".
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    """Update counter"""
+    app.logger.info(f"Request to update counter: {name}")
+    global COUNTERS
+    if name not in COUNTERS:
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+    COUNTERS[name] += 1
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+@app.route('/counters/<name>', methods=['GET'])
+def read_counter(name):
+    """Counter Read"""
+    app.logger.info(f"Request to read counter: {name}")
+    global COUNTERS
+    if name not in COUNTERS:
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+
---
+++ tests/test_counter.py
@@ -0,0 +1,67 @@
+"""
+Test Cases for Counter Web Service
+
+Create a service that can keep a track of multiple counters
+- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
+how to call the web service and assert what it should return.
+- The endpoint should be called /counters
+- When creating a counter, you must specify the name in the path.
+- Duplicate names must return a conflict error code.
+- The service must be able to update a counter by name.
+- The service must be able to read the counter
+"""
+
+import pytest
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
+@pytest.fixture()
+def client():
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndPoints:
+    """Test cases for Counter-related endpoints"""
+
+    def test_create_a_counter(self, client):
+        """It should create a counter"""
+        result = client.post('/counters/foo')
+        assert result.status_code == status.HTTP_201_CREATED
+
+    def test_duplicate_a_counter(self, client):
+        """Testing Duplicate"""
+        result = client.post('/counters/bar')
+        assert result.status_code == status.HTTP_201_CREATED
+        result = client.post('/counters/bar')
+        assert result.status_code == status.HTTP_409_CONFLICT
+
+    def test_update_counter(self, client):
+        """Testing Update"""
+        client.post('/counters/test')
+        result = client.put('/counters/test')
+        assert result.status_code == status.HTTP_200_OK
+        assert result.json == {'test': 1}
+        result = client.put('/counters/test')
+        assert result.status_code == status.HTTP_200_OK
+        assert result.json == {'test': 2}
+
+        result = client.put('/counters/nonexistent')
+        assert result.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_read_counter(self, client):
+        """Testing Read Counter"""
+        client.post('/counters/test2')
+        result = client.get('counters/test2')
+        assert result.status_code == status.HTTP_200_OK
+        assert result.json == {'test2':0}
+
+        result = client.get('counters/fake')
+        assert result.status_code == status.HTTP_404_NOT_FOUND
+
+
+
+
