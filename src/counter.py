55702f7df0886bd05b7d533a7a9b15a580b5acc9
root <root@NODE>
2024-09-19 03:46:37 -0700

    Fixing styling errors that failed workflow.

--- src/counter.py
+++ src/counter.py
@@ -3,8 +3,6 @@ from . import status
 
 app = Flask(__name__)
 
-
-
 COUNTERS = {}
 
 # We will use the app decorator and create a route called slash counters.
@@ -12,16 +10,18 @@ COUNTERS = {}
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
 @app.route('/counters/<name>', methods=['POST'])
+
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
 @app.route('/counters/<name>', methods=['PUT'])
+
 def update_counter(name):
     """Update counter"""
     app.logger.info(f"Request to update counter: {name}")
@@ -32,6 +32,7 @@ def update_counter(name):
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
 @app.route('/counters/<name>', methods=['GET'])
+
 def read_counter(name):
     """Counter Read"""
     app.logger.info(f"Request to read counter: {name}")
@@ -39,5 +40,3 @@ def read_counter(name):
     if name not in COUNTERS:
         return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
     return {name: COUNTERS[name]}, status.HTTP_200_OK
-
-
